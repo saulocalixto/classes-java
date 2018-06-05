f4a495c9636c10116c316c0e6ce07969db0c81ef
saulo <saulo@gmail.com>
2018-06-05 19:21:09 -0300

    add class

---
+++ BubbleSort.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+
+//https://www.devmedia.com.br/algoritmos-de-ordenacao-em-java/32693
+public class BubbleSort {
+    public static void main(String[] args) throws IOException {
+
+        int quantidade = 10000;
+        int[] vetor = new int[quantidade];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random()*quantidade);
+        }
+
+        long tempoInicial = System.currentTimeMillis();
+
+        bubbleSort(vetor);
+
+        long tempoFinal = System.currentTimeMillis();
+
+        System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
+
+    }
+
+    private static void bubbleSort(int vetor[]){
+        boolean troca = true;
+        int aux;
+        while (troca) {
+            troca = false;
+            for (int i = 0; i < vetor.length - 1; i++) {
+                if (vetor[i] > vetor[i + 1]) {
+                    aux = vetor[i];
+                    vetor[i] = vetor[i + 1];
+                    vetor[i + 1] = aux;
+                    troca = true;
+                }
+            }
+        }
+    }
+}
---
+++ InsertionSort.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+//https://www.devmedia.com.br/algoritmos-de-ordenacao-em-java/32693
+
+public class InsertionSort {
+    public static void main(String[] args) throws IOException {
+
+        int quantidade = 10000;
+        int[] vetor = new int[quantidade];
+
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) (Math.random()*quantidade);
+        }
+
+        long tempoInicial = System.currentTimeMillis();
+
+        insertionSort(vetor);
+
+        long tempoFinal = System.currentTimeMillis();
+
+        System.out.println("Executado em = " + (tempoFinal - tempoInicial) + " ms");
+
+    }
+
+    public static void insertionSort(int[] vetor) {
+        int j;
+        int key;
+        int i;
+
+        for (j = 1; j < vetor.length; j++)
+        {
+            key = vetor[j];
+            for (i = j - 1; (i >= 0) && (vetor[i] > key); i--)
+            {
+                vetor[i + 1] = vetor[i];
+            }
+            vetor[i + 1] = key;
+        }
+    }
+}
